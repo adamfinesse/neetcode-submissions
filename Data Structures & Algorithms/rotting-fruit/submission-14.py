6d36fa90e70577921eec298a04e52b9b487b4c5d
Adam <51472213+adamfinesse@users.noreply.github.com>
2026-04-07 06:56:01 -0400

    Add: rotting-fruit - submission-14

---
+++ Data Structures & Algorithms/rotting-fruit/submission-14.py
@@ -0,0 +1,47 @@
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = deque()
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        seen = set()
+        
+        def add_cell(r,c):
+            if r <0 or c < 0 or c >= COLS or r >= ROWS or (r,c) in seen or grid[r][c] == 0 or grid[r][c] == 2:
+                return
+            seen.add((r,c))
+            q.append((r,c))
+            return
+        #find our starting points
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 2:
+                    seen.add((r,c))
+                    add_cell(r+1,c)
+                    add_cell(r,c-1)
+                    add_cell(r,c+1)
+                    add_cell(r-1,c)
+
+        # while we have potential cells to visit, keep checking
+        minutes = 0
+        while q:
+            for i in range(len(q)):
+                r,c = q.popleft()
+
+                grid[r][c] = 2
+
+                add_cell(r+1,c)
+                add_cell(r,c-1)
+                add_cell(r,c+1)
+                add_cell(r-1,c)
+            
+            minutes +=1
+        
+        # check if there were any fruit we wernt able to process, meaning a -1 result
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    return -1
+       
+        return minutes
+        
