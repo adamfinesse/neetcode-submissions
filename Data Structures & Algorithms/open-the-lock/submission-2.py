001305732bd54a702f92e3f4a0c9112e3e4af073
Adam <51472213+adamfinesse@users.noreply.github.com>
2026-04-10 07:08:12 -0400

    Add: open-the-lock - submission-2

---
+++ Data Structures & Algorithms/open-the-lock/submission-2.py
@@ -0,0 +1,32 @@
+from collections import deque
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        q = deque(["0000"])
+        seen = set()
+        if "0000" in deadends:
+            return -1
+
+        min_turns = 0
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node == target:
+                    return min_turns
+                #positive
+                for j in range(4):
+                    dial_turned = node[0:j] + str((int(node[j]) + 1)%10) + node[j+1:]
+                    if dial_turned in deadends or dial_turned in seen:
+                        continue
+                    seen.add(dial_turned)
+                    q.append(dial_turned)
+                #negative
+                for j in range(4):
+                    dial_turned = node[0:j] + str((int(node[j]) - 1)%10) + node[j+1:]
+                    if dial_turned in deadends or dial_turned in seen:
+                        continue
+                    seen.add(dial_turned)
+                    q.append(dial_turned)
+            min_turns +=1
+
+        return -1
+                
