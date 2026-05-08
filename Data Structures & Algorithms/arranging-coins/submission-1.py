891362a992d78798f1dd5dcbdab152462fc7ef62
Adam <51472213+adamfinesse@users.noreply.github.com>
2026-05-08 07:07:32 -0400

    Add: arranging-coins - submission-1

---
+++ Data Structures & Algorithms/arranging-coins/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        complete_count = 0
+
+        row = 1
+        while n >= row:
+            if n-row < 0:
+                return complete_count
+            complete_count+=1
+            n -= row
+            row+=1
+
+        return complete_count
