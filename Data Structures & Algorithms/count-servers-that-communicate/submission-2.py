ea2690d97c31d9c3d5e03fa261859749e92d0d5c
Adam <51472213+adamfinesse@users.noreply.github.com>
2026-05-15 07:09:55 -0400

    Add: count-servers-that-communicate - submission-2

---
+++ Data Structures & Algorithms/count-servers-that-communicate/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+
+        r_cnt = [0]* len(grid)
+        for r in range(len(grid)):
+            servers_in_row = 0
+            for c in range(len(grid[r])):
+                if grid[r][c] == 1:
+                    servers_in_row +=1
+            if servers_in_row > 1:
+                r_cnt[r] += servers_in_row
+
+        print(r_cnt)
+        c_cnt = [0]* len(grid[0])
+        for c in range(len(grid[0])):
+            servers_in_col = 0
+            for r in range(len(grid)):
+                if grid[r][c] == 1:
+                    servers_in_col +=1
+            if servers_in_col > 1:
+                c_cnt[c] += servers_in_col
+        print(c_cnt)
+        
+        total = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                if grid[r][c] == 1 and (r_cnt[r] or c_cnt[c]):
+                    total+=1
+        return total
+
+        
+            
